678156e567c39ad9e87acf93bce785ffb49cd903
Mihai Alexandru <mciung18@student.aau.dk>
2021-05-19 20:24:13 +0200

    Add Wilcoxon test for Control, Mann-Whitney test for effect of starting conditions, boxplots

--- analysisStubHK.R
+++ analysisStubHK.R
@@ -3,7 +3,7 @@ library(Rmisc)
 library(gsheet)
 library(ggplot2)
 library(coin)
-
+library(ggpubr)
 
 df <- gsheet2tbl("https://docs.google.com/spreadsheets/d/1Q4xJHqa2e_rytQor89S_Bk1ZGHItm7OmFkVIkh8WMio/edit#gid=33771305")
 load("dfg.rda")
@@ -27,8 +27,15 @@ resbyMeasureByCondition <- df2 %>%
   )
 
 WilcoxonData <- df %>%
-  dplyr::select(PointBoy, `How much fun were you having in this condition?`) %>%
-  dplyr::rename(Fun = `How much fun were you having in this condition?`)
+  dplyr::select(PointBoy, `How much fun were you having in this condition?`, `I felt I was in control of the tricks and the landing.`) %>%
+  dplyr::rename(Fun = `How much fun were you having in this condition?`) %>%
+  dplyr::rename(Control = `I felt I was in control of the tricks and the landing.`)
+
+conditionOrderData <- df %>%
+  dplyr::select(`Starting Condition`, `How much fun were you having in this condition?`, `I felt I was in control of the tricks and the landing.`) %>%
+  dplyr::rename(Fun = `How much fun were you having in this condition?`) %>%
+  dplyr::rename(Control = `I felt I was in control of the tricks and the landing.`)
+
 
 pointBoyYes <- WilcoxonData %>%
   dplyr::filter(PointBoy=="Yes")
@@ -36,10 +43,46 @@ pointBoyYes <- WilcoxonData %>%
 pointBoyNo <- WilcoxonData %>%
   dplyr::filter(PointBoy=="No")
 
-wilcoxsign_test(pointBoyNo$`How much fun were you having in this condition?` ~ pointBoyYes$`How much fun were you having in this condition?`, distribution="exact")
+medianFunYes <- median(pointBoyYes$Fun, trim=0, na.rm = FALSE)
 
-ggplot(resbyMeasureByCondition, aes(x = Measure, y = mean, colour = Condition)) +
-  geom_point(position = position_dodge(width = 0.25)) +
-  geom_errorbar(aes(ymin = lci, ymax = uci), position = "dodge", width = 0.25) +
-  coord_flip()+theme_bw()
+medianFunNo <- median(pointBoyNo$Fun, trim=0, na.rm = FALSE)
 
+medianControlYes <- median(pointBoyYes$Control, trim=0, na.rm = FALSE)
+
+medianControlNo <- median(pointBoyNo$Control, trim=0, na.rm = FALSE)
+
+wilcoxsign_test(pointBoyNo$`Fun` ~ pointBoyYes$`Fun`, distribution="exact")
+
+wilcoxsign_test(pointBoyNo$`Control` ~ pointBoyYes$`Control`, distribution="exact")
+
+ggplot(WilcoxonData, aes(x = PointBoy, y = Fun, fill = PointBoy)) + 
+  geom_boxplot()
+
+ggplot(WilcoxonData, aes(x = PointBoy, y = Control, fill = PointBoy)) + 
+  geom_boxplot()
+
+condition1Start <- conditionOrderData %>%
+  dplyr::filter(`Starting Condition` == "pointBoy")
+
+condition2Start <- conditionOrderData %>%
+  dplyr::filter(`Starting Condition` == "nopointBoy")
+
+medianFunTotal <- median(df$`How much fun were you having in this condition?`, trim=0, na.rm=FALSE)
+
+medianFun1 <- median(condition1Start$Fun, trim=0, na.rm = FALSE)
+
+medianFun2 <- median(condition2Start$Fun, trim=0, na.rm = FALSE)
+
+medianControl1 <- median(condition1Start$Control, trim=0, na.rm = FALSE)
+
+medianControl2 <- median(condition2Start$Control, trim=0, na.rm = FALSE)
+
+wilcox.test(condition1Start$`Fun`, condition2Start$`Fun`)
+
+wilcox.test(condition1Start$`Control`, condition2Start$`Control`)
+
+ggplot(conditionOrderData, aes(x = `Starting Condition`, y = Fun, fill = `Starting Condition`)) + 
+  geom_boxplot()
+
+ggplot(conditionOrderData, aes(x = `Starting Condition`, y = Control, fill = `Starting Condition`)) + 
+  geom_boxplot()
